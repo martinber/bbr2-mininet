7804b250d7ac49d6fcec2e1617d26deec74a6877
Martin Bernardi <martin@mbernardi.com.ar>
2020-05-25 19:19:59 -0400

    Parsing de captcp statistic casi andando

--- prueba-mininet/prueba.py
+++ prueba-mininet/prueba.py
@@ -36,7 +36,7 @@ class MiHost(Host):
         Corre un comando en el fondo y devuelve su PID.
         """
         cmd = cmd + ("&",)
-        self.cmd(*cmd)
+        print(self.cmd(*cmd))
         return int(self.cmd("echo $!"))
 
     def killPid(self, pid):
@@ -44,6 +44,30 @@ class MiHost(Host):
         Mata un proceso dado su PID.
         """
         self.cmd("kill", pid)
+
+def parse_captcp_stat(text):
+
+    def group(iterator, count):
+        itr = iter(iterator)
+        while True:
+            yield tuple([itr.next() for i in range(count)])
+
+
+    flow = ("0.1", 0)
+
+    for lines in group(iter(text.splitlines()), 3):
+        flow1 = lines[0].strip().split()[1]
+        flow2 = lines[1].strip().split()[1]
+        print(lines[2].strip().split())
+        data1 = int(lines[2].strip().split()[3])
+        data2 = int(lines[2].strip().split()[8])
+
+        if data1 > flow[1]:
+            flow = (flow1, data1)
+        if data2 > flow[1]:
+            flow = (flow2, data2)
+
+    return flow[0]
 
 def myNetwork():
 
@@ -59,6 +83,8 @@ def myNetwork():
 
     net.addLink(h1, s1)
     net.addLink(h2, s1)
+
+    #net.addLink( host, switch, bw=10, delay='5ms', loss=2, max_queue_size=1000, use_htb=True )
 
     net.build()
 
@@ -78,19 +104,21 @@ def myNetwork():
     # --------------------------------------------------------------------------
     # Crear carpetas temporales y moverse ahi
 
-    h1.cmd("rm -r /tmp/mininet")
-    h1.cmd("mkdir -p /tmp/mininet/h1")
-    h1.cmd("cd /tmp/mininet/h1")
+    h1.cmd("rm -r /var/tmp/mininet")
+    h1.cmd("mkdir -p /var/tmp/mininet/h1")
+    h1.cmd("cd /var/tmp/mininet/h1")
 
-    h2.cmd("mkdir -p /tmp/mininet/h2")
-    h2.cmd("cd /tmp/mininet/h2")
+    h2.cmd("mkdir -p /var/tmp/mininet/h2")
+    h2.cmd("cd /var/tmp/mininet/h2")
 
     h1.cmd("mkdir -p ~/resultados/")
 
     # --------------------------------------------------------------------------
     # Servidor HTTP
 
-    h1.cmd("cd /etc/apt/")
+    # pushd hace cd a una carpeta. Lo que tiene de bueno es que cuando use popd
+    # puedo volver a la carpeta anterior
+    h1.cmd("pushd /etc/apt/")
     h1.cmdPrint("ls")
 
     pid_py3 = h1.bgCmd("python3 -m http.server 8080")
@@ -101,21 +129,29 @@ def myNetwork():
     h2.cmdPrint("ls")
 
     h1.killPid(pid_py3)
+    h1.cmd("popd")
 
     # --------------------------------------------------------------------------
     # Iperf3
     # Cliente manda a servidor
 
-    pid_iperf = h2.bgCmd("iperf3 -s")
+    pid_iperf = h2.bgCmd("iperf -s")
     time.sleep(1)
 
+    pid_tcpdump = h1.cmdPrint("ip a")
     pid_tcpdump = h1.bgCmd("tcpdump -i h1-eth0 -w ./trace.pcap")
-    h1.cmdPrint("iperf3 -c 10.0.0.2")
+    h1.cmdPrint("iperf -c 10.0.0.2")
 
     h1.killPid(pid_tcpdump)
     h2.killPid(pid_iperf)
 
-    h1.cmdPrint("captcp statistic trace.pcap")
+    h1.cmdPrint("pwd")
+    h2.cmdPrint("pwd")
+
+    salida = h1.cmd("captcp statistic ./trace.pcap | grep -E 'Flow|Data application layer'")
+    print(salida)
+    print parse_captcp_stat(salida)
+
 
     # --------------------------------------------------------------------------
     # Ver que se hayan cerrado los procesos
@@ -131,4 +167,3 @@ def myNetwork():
 if __name__ == '__main__':
     setLogLevel( 'info' )
     myNetwork()
-
