489bc04897aa78ff3c71c2878cd39cee485a96e6
Martin Bernardi <martin@mbernardi.com.ar>
2020-05-28 17:19:43 -0400

    Empezando a definir muchas pruebas

--- tcp-mininet/tcp_mininet.py
+++ tcp-mininet/tcp_mininet.py
@@ -49,11 +49,14 @@ class MiHost(Host):
         """
         self.cmdPrint("kill -INT {}".format(pid))
 
-def myNetwork():
+def run_test(test):
 
-    net = Mininet( topo=None,
-                   build=False,
-                   ipBase='10.0.0.0/8')
+    # ------------------------------------------------------------------
+    # Crear topologia
+    
+    net = Mininet(topo=None,
+                  build=False,
+                  ipBase='10.0.0.0/8')
 
     info( '*** Iniciando\n')
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch, failMode='standalone')
@@ -61,10 +64,8 @@ def myNetwork():
     h1 = net.addHost('h1', cls=MiHost, ip='10.0.0.1', defaultRoute=None)
     h2 = net.addHost('h2', cls=MiHost, ip='10.0.0.2', defaultRoute=None)
 
-    net.addLink(h1, s1)
-    net.addLink(h2, s1)
-
-    #net.addLink( host, switch, bw=10, delay='5ms', loss=2, max_queue_size=1000, use_htb=True )
+    net.addLink(h1, s1, cls=TCLink, **test.get_link_params())
+    net.addLink(h2, s1, cls=TCLink, **test.get_link_params())
 
     net.build()
 
@@ -73,122 +74,128 @@ def myNetwork():
 
     net.get('s1').start([])
 
-    info( '*** Realizar pruebas\n')
-
     # --------------------------------------------------------------------------
     # Inicializar
 
     h1.cmd("modprobe tcp_bbr")
-    h2.cmd("modprobe tcp_bbr")
+    h2.cmd("modprobe tcp_bbr2")
 
     # --------------------------------------------------------------------------
     # Crear carpetas temporales y moverse ahi
 
-    h1.cmd("rm -r /var/tmp/mininet")
-    h1.cmd("mkdir -p /var/tmp/mininet/h1")
-    h1.cmd("cd /var/tmp/mininet/h1")
+    h1.cmd("rm -r {}".format(test.folder))
+    h1.cmd("mkdir -p {}/h1".format(test.folder))
+    h1.cmd("cd {}/h1".format(test.folder))
 
-    h2.cmd("mkdir -p /var/tmp/mininet/h2")
-    h2.cmd("cd /var/tmp/mininet/h2")
+    h2.cmd("mkdir -p {}/h2".format(test.folder))
+    h2.cmd("cd {}/h2".format(test.folder))
 
     h1.cmd("mkdir -p ~/resultados/")
-
+    
     # --------------------------------------------------------------------------
-    # Servidor HTTP
+    # Empezar a loguear info
 
-    # pushd hace cd a una carpeta. Lo que tiene de bueno es que cuando use popd
-    # puedo volver a la carpeta anterior
-    #h1.cmd("pushd /etc/apt/")
-    #h1.cmdPrint("ls")
+    with open(test.log_path, "w") as log_file:
 
-    #pid_py3 = h1.bgCmd("python3 -m http.server 8080")
-    #time.sleep(1)
+        log_file.write(str(test) + "\n")
+        log_file.write("------------------------------\n")
 
-    #h2.cmdPrint("ls")
-    #h2.cmdPrint("wget -r http://10.0.0.1:8080")
-    #h2.cmdPrint("ls")
+        # --------------------------------------------------------------------------
+        # Servidor HTTP
 
-    #h1.killPid(pid_py3)
-    #h1.cmd("popd")
+        # pushd hace cd a una carpeta. Lo que tiene de bueno es que cuando use popd
+        # puedo volver a la carpeta anterior
+        #h1.cmd("pushd /etc/apt/")
+        #h1.cmdPrint("ls")
 
-    # --------------------------------------------------------------------------
-    # Iperf3
-    # Cliente manda a servidor
+        #pid_py3 = h1.bgCmd("python3 -m http.server 8080")
+        #time.sleep(1)
 
-    pid_iperf = h2.bgCmd("iperf -s")
-    time.sleep(1)
+        #h2.cmdPrint("ls")
+        #h2.cmdPrint("wget -r http://10.0.0.1:8080")
+        #h2.cmdPrint("ls")
 
-    pid_tcpdump = h1.bgCmd("tcpdump -i h1-eth0 -w ./trace.pcap")
-    h1.cmd("mkdir ./captcp_ss")
-    pid_captcp_ss = h1.bgCmd("captcp socketstatistic -s 10 -o ./captcp_ss") # 10Hz
-    h1.cmdPrint("iperf -c 10.0.0.2")
+        #h1.killPid(pid_py3)
+        #h1.cmd("popd")
 
-    time.sleep(1)
-    
-    print "Parando tcpdump"
-    h1.killPid(pid_tcpdump)
-    print "Parando captcp"
-    h1.killPid(pid_captcp_ss)
-    time.sleep(2)
-    print "Parando iperf"
-    h2.killPid(pid_iperf)
+        # --------------------------------------------------------------------------
+        # Iperf3
+        # Cliente manda a servidor
 
-    print "Graficando socketstatistic"
-    
-    h1.cmd("pushd ./captcp_ss/*:5001")
-    
-    h1.cmd("pushd ./cwnd-ssthresh")
-    h1.cmdPrint("pwd")
-    h1.cmdPrint("make")
-    h1.cmd("popd")
-    
-    h1.cmd("pushd ./rtt")
-    h1.cmdPrint("make")
-    h1.cmd("popd")
-    
-    h1.cmd("pushd ./skmem")
-    h1.cmdPrint("make")
-    h1.cmd("popd")
-    
-    h1.cmd("popd")
-    
-    print "Determinando el numero de flow capturado"
+        pid_iperf = h2.bgCmd("iperf -s")
+        time.sleep(1)
 
-    salida = h1.cmd("captcp statistic ./trace.pcap | grep -E 'Flow|Data application layer' | ansi2txt")
-    flow = parse.parse_captcp_stat(salida)
-    
-    print "Graficando throughput"
+        pid_tcpdump = h1.bgCmd("tcpdump -s 96 -i h1-eth0 -w ./trace.pcap")
+        h1.cmd("mkdir ./captcp_ss")
+        pid_captcp_ss = h1.bgCmd("captcp socketstatistic -s 50 -o ./captcp_ss") # 10Hz
+        h1.cmdPrint("iperf -c 10.0.0.2 -t 20")
 
-    h1.cmd('mkdir -p "./captcp_throughput"')
-    h1.cmd(
-        "captcp throughput -s {sample_len} -i -u Mbit \
-        -f {flow} -o {output_dir} {pcap}".format(
-            sample_len="0.1",
-            flow=flow,
-            output_dir="./captcp_throughput",
-            pcap="./trace.pcap"
+        time.sleep(1)
+        
+        print "Parando tcpdump"
+        h1.killPid(pid_tcpdump)
+        print "Parando captcp"
+        h1.killPid(pid_captcp_ss)
+        time.sleep(2)
+        print "Parando iperf"
+        h2.killPid(pid_iperf)
+
+        print "Graficando socketstatistic"
+        
+        h1.cmd("pushd ./captcp_ss/*:5001")
+        
+        h1.cmd("pushd ./cwnd-ssthresh")
+        h1.cmdPrint("pwd")
+        h1.cmdPrint("make")
+        h1.cmd("popd")
+        
+        h1.cmd("pushd ./rtt")
+        h1.cmdPrint("make")
+        h1.cmd("popd")
+        
+        h1.cmd("pushd ./skmem")
+        h1.cmdPrint("make")
+        h1.cmd("popd")
+        
+        h1.cmd("popd")
+        
+        print "Determinando el numero de flow capturado"
+
+        salida = h1.cmd("captcp statistic ./trace.pcap | grep -E 'Flow|Data application layer' | ansi2txt")
+        flow = parse.parse_captcp_stat(salida)
+        
+        print "Graficando throughput"
+
+        h1.cmd('mkdir -p "./captcp_throughput"')
+        h1.cmd(
+            "captcp throughput -s {sample_len} -i -u Mbit \
+            -f {flow} -o {output_dir} {pcap}".format(
+                sample_len="0.1",
+                flow=flow,
+                output_dir="./captcp_throughput",
+                pcap="./trace.pcap"
+            )
         )
-    )
-    h1.cmd("pushd ./captcp_throughput")
-    h1.cmd("make")
-    h1.cmd("popd")
-    
-    print "Graficar inflight"
+        h1.cmd("pushd ./captcp_throughput")
+        h1.cmd("make")
+        h1.cmd("popd")
+        
+        print "Graficar inflight"
 
-    h1.cmd('mkdir -p "./captcp_inflight"')
-    h1.cmd(
-        "captcp inflight -i -f {flow} -o {output_dir} {pcap}".format(
-            flow=flow,
-            output_dir="./captcp_inflight",
-            pcap="./trace.pcap"
+        h1.cmd('mkdir -p "./captcp_inflight"')
+        h1.cmd(
+            "captcp inflight -i -f {flow} -o {output_dir} {pcap}".format(
+                flow=flow,
+                output_dir="./captcp_inflight",
+                pcap="./trace.pcap"
+            )
         )
-    )
-    h1.cmd("pushd ./captcp_inflight")
-    h1.cmd("make")
-    h1.cmd("popd")
+        h1.cmd("pushd ./captcp_inflight")
+        h1.cmd("make")
+        h1.cmd("popd")
 
-    # --------------------------------------------------------------------------
-    # Ver que se hayan cerrado los procesos
+        # --------------------------------------------------------------------------
+        # Ver que se hayan cerrado los procesos
 
     time.sleep(1)
     if h1.waiting:
@@ -197,7 +204,76 @@ def myNetwork():
         print "Proceso de h2 no salio"
     if not h1.waiting and not h2.waiting:
         print "Los procesos de h1 y h2 terminaron"
+        
+    net.stop()
+        
+class TestDef:
+    
+    total_tests = 0 # Llevar cuenta de cantidad de tests
+    
+    def __init__(
+        self,
+        tcp_cc, # Congestion control: reno, cubic, bbr, bbr2
+        bw, # Ancho de banda de enlaces en kbps?
+        delay, # Delay de enlaces en ms, el RTT deberia ser el doble
+        max_queue_size, # Tamano de cola de cada enlace
+    ):
+        TestDef.total_tests += 1
+        
+        # Numero de test
+        self.test_num = TestDef.total_tests
+        
+        self.tcp_cc = tcp_cc
+        self.bw = bw
+        self.delay = delay
+        self.max_queue_size = max_queue_size
+        
+        # Nombre unico, pensado para usarse como nombre de archivo
+        self.name = "{tcp_cc}_{bw}k_{delay}ms_{queue}pkt".format(
+            tcp_cc=tcp_cc,
+            bw=bw,
+            delay=delay,
+            queue=max_queue_size,
+        )
+        
+        self.folder = "/var/tmp/mininet/{}/".format(self.name)
+        
+        self.log_path = "{}/log.txt".format(self.folder)
+        
+    def get_link_params(self):
+        """
+        Devuelve parametros a usar en los links
+        """
+        return {
+            "bw": self.bw,
+            "delay": "{}ms".format(self.delay),
+            "max_queue_size": self.max_queue_size,
+        }
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
-    myNetwork()
+    
+    
+    tests = [
+        TestDef(
+            tcp_cc="reno",
+            bw=10000,
+            delay=100,
+            max_queue_size=100,
+        ),
+        TestDef(
+            tcp_cc="reno",
+            bw=100000,
+            delay=100,
+            max_queue_size=100,
+        ),
+        TestDef(
+            tcp_cc="reno",
+            bw=1000000,
+            delay=100,
+            max_queue_size=100,
+        ),
+    ]
+    
+    for t in tests:
+        run_test(t)
