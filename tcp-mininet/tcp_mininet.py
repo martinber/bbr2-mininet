b8f003e7b877cd3c8217be0f14f3e95cbd7351e0
Martin Bernardi <martin@mbernardi.com.ar>
2020-06-08 18:22:37 -0400

    Agregado parametro limit, arreglado parametro loss, guardar graficos en otra carpeta, y varios

--- tcp-mininet/tcp_mininet.py
+++ tcp-mininet/tcp_mininet.py
@@ -16,6 +16,7 @@ import parse
 import time
 import signal
 import shutil
+import itertools
 
 class MiHost(Host):
     """
@@ -112,19 +113,28 @@ def run_test(test):
     netem.cmd("tc qdisc del dev {intf} root".format(
             intf=intf
         ))
+    if test.loss > 0:
+        loss = "loss {}% ".format(test.loss),
+    else:
+        loss = ""
     netem.cmdPrint(("tc qdisc add dev {intf} root handle 1: netem "
-               "delay {delay}ms {jitter}ms {corr}%").format(
+                "{loss} delay {delay}ms {jitter}ms {corr}%").format(
             intf=intf,
+            loss=loss,
             delay=test.delay,
             jitter=test.jitter,
             corr=test.corr,
         ))
+    if test.limit:
+        buf_params = "limit {}".format(test.limit)
+    else:
+        buf_params = "latency {}ms".format(test.latency)
     netem.cmdPrint(("tc qdisc add dev {intf} parent 1: handle 2: tbf "
-               "rate {bw}mbit burst {burst}kb latency {latency}ms").format(
+               "rate {bw}mbit burst {burst}kb {buf_params}").format(
             intf=intf,
             bw=test.bw,
             burst=test.burst,
-            latency=test.latency,
+            buf_params=buf_params,
         ))
 
     # --------------------------------------------------------------------------
@@ -178,8 +188,8 @@ def run_test(test):
 
         pid_tcpdump = h1.bgCmd("tcpdump -i h1-eth0 -w ./trace.pcap")
         h1.cmd("mkdir ./captcp_ss")
-        pid_captcp_ss = h1.bgCmd("captcp socketstatistic -s 50 -o ./captcp_ss") # 10Hz
-        h1.logCmd(log_file, "iperf -c 11.0.0.10 -t 20")
+        pid_captcp_ss = h1.bgCmd("captcp socketstatistic -s 100 -o ./captcp_ss") # 100Hz
+        h1.logCmd(log_file, "iperf -c 11.0.0.10 -t {}".format(test.duration))
 
         time.sleep(1)
         
@@ -197,10 +207,14 @@ def run_test(test):
         
         h1.cmd("pushd ./cwnd-ssthresh")
         h1.logCmd(log_file, "make")
+        h1.cmd("mkdir -p {}/ssthreshs/".format(test.resfolder))
+        h1.cmd("cp *.pdf {}/ssthreshs/{}.pdf".format(test.resfolder, test.name))
         h1.cmd("popd")
         
         h1.cmd("pushd ./rtt")
         h1.logCmd(log_file, "make")
+        h1.cmd("mkdir -p {}/rtts/".format(test.resfolder))
+        h1.cmd("cp *.pdf {}/rtts/{}.pdf".format(test.resfolder, test.name))
         h1.cmd("popd")
         
         h1.cmd("pushd ./skmem")
@@ -220,7 +234,7 @@ def run_test(test):
         h1.cmd(
             "captcp throughput -ps {sample_len} -i -u Mbit \
             -f {flow} -o {output_dir} {pcap}".format(
-                sample_len="0.1",
+                sample_len="0.02",
                 flow=flow,
                 output_dir="./captcp_throughput",
                 pcap="./trace.pcap"
@@ -228,6 +242,8 @@ def run_test(test):
         )
         h1.cmd("pushd ./captcp_throughput")
         h1.cmd("make")
+        h1.cmd("mkdir -p {}/throughputs/".format(test.resfolder))
+        h1.cmd("cp *.pdf {}/throughputs/{}.pdf".format(test.resfolder, test.name))
         h1.cmd("popd")
         
         print "Graficar inflight"
@@ -242,6 +258,8 @@ def run_test(test):
         )
         h1.cmd("pushd ./captcp_inflight")
         h1.cmd("make")
+        h1.cmd("mkdir -p {}/inflights/".format(test.resfolder))
+        h1.cmd("cp *.pdf {}/inflights/{}.pdf".format(test.resfolder, test.name))
         h1.cmd("popd")
         
         h1.cmd("rm ./trace.pcap")
@@ -269,10 +287,12 @@ class TestDef:
         bw=100, # Ancho de banda de enlaces en Mbitps
         burst=32, # Tamano del burs en kbytes, ver manual de tbf
         latency=50, # Maximo delay del buffer en ms
+        limit=None, # Tamano del buffer en bytes, es una alternativa a latency
         delay=0, # Delay de enlaces en ms, el RTT deberia ser el doble
         jitter=0, # Jitter en ms
-        corr=25, # Correlacin del jitter en porcentaje (1 = 1%)
+        corr=25, # Correlacion del jitter en porcentaje (1 = 1%)
         loss=0, # Perdida en porcentaje (1 = 1%)
+        duration=20 # Duracion de prueba en segundos
     ):
         TestDef.total_tests += 1
         
@@ -283,17 +303,20 @@ class TestDef:
         self.bw = bw
         self.burst = burst
         self.latency = latency
+        self.limit = limit
         self.delay = delay
         self.jitter = jitter
         self.corr = corr
         self.loss = loss
+        self.duration = duration
         
         # Nombre unico, pensado para usarse como nombre de archivo
-        self.name = "{test_num}_{tcp_cc}_{bw}mbps_{delay}~{jitter}ms_{loss}%".format(
+        self.name = "{test_num}_{tcp_cc}_{bw}mbps_buf{latency}ms_limit{limit}_{delay}~{jitter}ms_{loss}%".format(
             tcp_cc=tcp_cc,
             bw=bw,
             burst=burst,
             latency=latency,
+            limit=limit,
             delay=delay,
             jitter=jitter,
             corr=corr,
@@ -303,6 +326,8 @@ class TestDef:
         
         self.folder = "/var/tmp/mininet/{}/".format(self.name)
         
+        self.resfolder = "/var/tmp/mininet/res/"
+        
         self.log_path = "{}/log.txt".format(self.folder)
         
     def __str__(self):
@@ -310,25 +335,103 @@ class TestDef:
         Se imprime a si mismo para loguearse
         """
         return self.name
+        
+def prueba_1():
+    
+    tests = []
+    
+    tcp_ccs = ["reno", "cubic", "bbr", "bbr2"]
+    bws = [10, 1000] # Mbps
+    bursts = [32] # kb
+    latencies = [50, 200] # ms
+    delays = [20, 100] # ms
+    jitters = [0] # ms
+    corrs = [25] # %
+    losses = [0, 1] # %
+    
+    for tcp_cc, bw, burst, latency, delay, jitter, corr, loss in itertools.product(
+            tcp_ccs, bws, bursts, latencies, delays, jitters, corrs, losses):
+        
+        tests.append(TestDef(
+            tcp_cc=tcp_cc,
+            bw=bw, # Mbps
+            burst=burst, # kb
+            latency=latency, # ms
+            delay=delay, # ms
+            jitter=jitter, # ms
+            corr=corr, # %
+            loss=loss, # %
+        ))
+    
+    for t in tests:
+        run_test(t)
+        
+def prueba_2():
+    
+    tests = []
+    
+    tcp_ccs = ["bbr", "bbr2"]
+    bws = [100] # Mbps
+    latencies = [100] # ms
+    delays = [100] # ms
+    jitters = [0] # ms
+    corrs = [25] # %
+    losses = [0] # %
+    
+    for tcp_cc, bw, latency, delay, jitter, corr, loss in itertools.product(
+            tcp_ccs, bws, latencies, delays, jitters, corrs, losses):
+        
+        tests.append(TestDef(
+            tcp_cc=tcp_cc,
+            bw=bw, # Mbps
+            burst=int(bw*1000/250), # kb, https://unix.stackexchange.com/a/100797
+            latency=latency, # ms
+            delay=delay, # ms
+            jitter=jitter, # ms
+            corr=corr, # %
+            loss=loss, # %
+            duration=10 # s
+        ))
+    
+    for t in tests:
+        run_test(t)
+        
+def prueba_3():
+    
+    tests = []
+    
+    tcp_ccs = ["bbr", "bbr2"]
+    bws = [100] # Mbps
+    latencies = [30, 60, 90] # ms
+    delays = [5, 30, 60, 90] # ms
+    jitters = [0] # ms
+    corrs = [25] # %
+    losses = [0] # %
+    
+    for tcp_cc, bw, latency, delay, jitter, corr, loss in itertools.product(
+            tcp_ccs, bws, latencies, delays, jitters, corrs, losses):
+        
+        BDP = int(bw*100000000 * delay/1000) # bits/s * segundos
+        
+        tests.append(TestDef(
+            tcp_cc=tcp_cc,
+            bw=bw, # Mbps
+            burst=int(bw*1000/250), # kb, https://unix.stackexchange.com/a/100797
+            limit=int(10*BDP/8), # bytes
+            delay=delay, # ms
+            jitter=jitter, # ms
+            corr=corr, # %
+            loss=loss, # %
+            duration=3 # s
+        ))
+    
+    for t in tests:
+        run_test(t)
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
     
     shutil.rmtree("/var/tmp/mininet", ignore_errors=True)
     
-    tests = [
-        TestDef(
-            tcp_cc="reno",
-            bw=10, # Mbps
-            burst=32, # kb
-            latency=50, # ms
-            delay=100, # ms
-            jitter=0, # ms
-            corr=25, # %
-            loss=None, # %
-        ),
-        
-    ]
-    
-    for t in tests:
-        run_test(t)
+    prueba_3()
+
